e612d64451cc9f54784e68bd3837731b98f7817a
1060aorus2 <nailazukanovic@gmail.com>
2022-03-20 15:52:38 +0100

    Add project files.

---
+++ SuperHeroAPI/Data/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Data.Entity;
+
+namespace SuperHeroAPI.Data
+{
+    public class DataContext : DbContext 
+    {
+        public Datcontext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<SuperHero> SuperHeroes { get; set; }
+    }
+}
